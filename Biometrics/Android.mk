44abbfc5147993a93121d6ff3ae20e78b3af5211
TheScarastic <warabhishek@gmail.com>
2018-08-16 14:52:58 +0530

    Revert "msm8956-common: Update biometrics for pie"
    This reverts commit 27dc9d72747cc40137614e8a4b142e99e3778115.

--- Biometrics/Android.mk
+++ Biometrics/Android.mk
@@ -23,7 +23,7 @@ LOCAL_SHARED_LIBRARIES := \
     libhardware \
     libutils \
     libhwbinder \
-    libkeystore_aidl \
+    libkeystore_binder \
     android.hardware.biometrics.fingerprint@2.1 \
 
 include $(BUILD_EXECUTABLE)
