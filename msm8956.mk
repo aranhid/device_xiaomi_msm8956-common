217f8bae3c619e4f0da2d4412de0a53a617dbb9c
HarukeyUA <inazarrusnak@gmail.com>
2019-03-03 10:19:54 +0000

    msm8956: Improve Bluetooth + WiFi coexistence
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- msm8956.mk
+++ msm8956.mk
@@ -124,7 +124,8 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-service 
+    android.hardware.bluetooth@1.0-service \
+    libbt-vendor
 
 # Camera
 PRODUCT_PACKAGES += \
